319df40ea4b9a7271fe962e3aa5628a5865907bb
Mulenga Bowa <mulengabowa53@gmail.com>
2020-05-09 00:02:24 +0200

    refactor client

--- src/Client.php
+++ src/Client.php
@@ -12,13 +12,11 @@ class Client
     private const APP_SERVICE = 'crud';
 
     private $address;
-    private $mnemonic;
     private $endpoint;
     private $chainId;
     private $uuid;
-    private $gasInfo;
-    private $accountInfo;
     private $httpClient;
+    private $cosmos;
 
 
     /**
@@ -33,11 +31,7 @@ class Client
         $this->endpoint = isset($endpoint) ? $endpoint : 'http://localhost:1317';
         $this->uuid = isset($uuid) ? $uuid : $this->address;
 
-        $this->httpClient = new \GuzzleHttp\Client();
-        $this->key = Utils::getECKey($this->mnemonic);
-
-        $this->validateAddress();
-        $this->setAccountDetails();
+        $this->cosmos = new Cosmos($address, $mnemonic, $this->endpoint, $this->chainId); 
     }
 
     public function create(string $key, string $value, array $gasInfo, array $leaseInfo = null): void
@@ -45,7 +39,7 @@ class Client
         $lease = Utils::convertLease($leaseInfo);
         $this->validateLease($lease);
 
-        $this->sendTransaction(
+        $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/create',
             $this->buildParams([ 'Key' => $key, 'Value' => $value, 'Lease' => (string) $lease ]),
@@ -58,7 +52,7 @@ class Client
         $lease = Utils::convertLease($leaseInfo);
         $this->validateLease($lease);
 
-        $this->sendTransaction(
+        $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/update',
             $this->buildParams([ 'Key' => $key, 'Value' => $value, 'Lease' => (string) $lease ]),
@@ -71,12 +65,12 @@ class Client
     {
         $path = $prove ? 'pread' : 'read';
         $url = self::APP_SERVICE . "/" . $path . "/" . $this->uuid  . "/" . $key;
-        return $this->query($url)['result']['value'];
+        return $this->cosmos->query($url)['result']['value'];
     }
 
     public function txRead(string $key, array $gasInfo): string
     {
-        return $this->sendTransaction(
+        return $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/read',
             $this->buildParams([ 'Key' => $key ]),
@@ -86,7 +80,7 @@ class Client
 
     public function delete(string $key, array $gasInfo): void
     {
-        $this->sendTransaction(
+        $this->cosmos->sendTransaction(
             'DELETE',
             self::APP_SERVICE . '/delete',
             $this->buildParams([ 'Key' => $key ]),
@@ -97,12 +91,12 @@ class Client
     public function has(string $key): bool
     {
         $url = self::APP_SERVICE . '/has/' . $this->uuid . '/' . $key;
-        return $this->query($url)['result']['has'];
+        return $this->cosmos->query($url)['result']['has'];
     }
 
     public function txHas(string $key, array $gasInfo): bool
     {
-        return $this->sendTransaction(
+        return $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/has',
             $this->buildParams([ 'key' => $key ]),
@@ -113,12 +107,12 @@ class Client
     public function keys(): array
     {
         $url = self::APP_SERVICE . '/keys/' . $this->uuid;
-        return $this->query($url)['result']['keys'];
+        return $this->cosmos->query($url)['result']['keys'];
     }
 
     public function txkeys(array $gasInfo): array
     {
-        return $this->sendTransaction(
+        return $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/keys',
             $this->buildParams([]),
@@ -128,7 +122,7 @@ class Client
 
     public function rename(string $key, string $newKey, array $gasInfo): void
     {
-        $this->sendTransaction(
+        $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/rename',
             $this->buildParams([ 'Key' => $key, 'NewKey' => $newKey ]),
@@ -139,12 +133,12 @@ class Client
     public function count(): int
     {
         $url = self::APP_SERVICE . '/count/' . $this->uuid;
-        return $this->query($url)['result']['count'];
+        return $this->cosmos->query($url)['result']['count'];
     }
 
     public function txCount(array $gasInfo): int
     {
-        return $this->sendTransaction(
+        return $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/count',
             $this->buildParams([]),
@@ -154,7 +148,7 @@ class Client
 
     public function deleteAll(array $gasInfo): void
     {
-        $this->sendTransaction(
+        $this->cosmos->sendTransaction(
             'DELETE',
             self::APP_SERVICE . '/deleteall',
             $this->buildParams([]),
@@ -165,12 +159,12 @@ class Client
     public function keyValues(): array
     {
         $url = self::APP_SERVICE . '/keyvalues/' . $this->uuid;
-        return $this->query($url)['result']['keyvalues'];
+        return $this->cosmos->query($url)['result']['keyvalues'];
     }
 
     public function txKeyValues(array $gasInfo): array
     {
-        return $this->sendTransaction(
+        return $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/keyvalues',
             $this->buildParams([]),
@@ -180,7 +174,7 @@ class Client
 
     public function multiUpdate(array $keyValues, array $gasInfo): void
     {
-        $this->sendTransaction(
+        $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/multiupdate',
             $this->buildParams([ 'KeyValues' => $keyValues ]),
@@ -191,13 +185,13 @@ class Client
     public function getLease(string $key): string
     {
         $url = self::APP_SERVICE . '/getlease/' . $this->uuid . '/' . $key;
-        $lease = (int) $this->query($url)['result']['lease'];
+        $lease = (int) $this->cosmos->query($url)['result']['lease'];
         return (string) ($lease * self::BLOCK_TIME_IN_SECONDS);
     }
 
     public function txGetLease(string $key, array $gasInfo): string
     {
-        $lease = (int) $this->sendTransaction(
+        $lease = (int) $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/getlease',
             $this->buildParams([ 'Key' => $key ]),
@@ -212,7 +206,7 @@ class Client
         $lease = Utils::convertLease($leaseInfo);
         $this->validateLease($lease);
 
-        $this->sendTransaction(
+        $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/renewlease',
             $this->buildParams([ 'Key' => $key, 'Lease' => (string) $lease ]),
@@ -225,7 +219,7 @@ class Client
         $lease = Utils::convertLease($leaseInfo);
         $this->validateLease($lease);
 
-        $this->sendTransaction(
+        $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/renewleaseall',
             $this->buildParams([ 'Lease' => (string) $lease ]),
@@ -236,12 +230,12 @@ class Client
     public function getNShortestLease(int $n): array
     {
         $url = self::APP_SERVICE . '/getnshortestlease/' . $this->uuid . '/' . $n;
-        return $this->query($url)['result']['keyleases'];
+        return $this->cosmos->query($url)['result']['keyleases'];
     }
 
     public function txGetNShortestLease(int $n, array $gasInfo): array
     {
-        return $this->sendTransaction(
+        return $this->cosmos->sendTransaction(
             'POST',
             self::APP_SERVICE . '/getnshortestlease',
             $this->buildParams([ 'N' => (string) $n ]),
@@ -252,35 +246,17 @@ class Client
     public function account(): array
     {
         $url = 'auth/accounts/' . $this->address;
-        return $this->query($url)['result']['value'];
+        return $this->cosmos->query($url)['result']['value'];
     }
 
     public function version(): string
     {
-        return $this->query('node_info')['application_version']['version'];
+        return $this->cosmos->query('node_info')['application_version']['version'];
     }
 
     public function setHttpClient($httpClient)
     {
-        $this->httpClient = $httpClient;
-    }
-
-    private function setAccountDetails()
-    {
-        $data = $this->account();
-
-        $this->accountInfo['account_number'] = $data['account_number'];
-        $this->accountInfo['sequence'] = $data['sequence'];
-    }
-
-    private function validateAddress()
-    {
-        $retrievedAddress = Utils::getAddressFromPublicKey($this->getPublicKey());
-
-        if ($this->address !== $retrievedAddress)
-        {
-            throw new Exception\AddressValidationFailedException();
-        }
+        $this->cosmos->setHttpClient($httpClient);
     }
 
     private function validateLease(int $blocks)
@@ -288,135 +264,6 @@ class Client
         if ($blocks < 0) throw new Exception\InvalidLeaseException();
     }
 
-    private function request($method, $url, $data = [])
-    {
-        $res = $this->httpClient->request($method, $url, $data);
-        return Utils::jsonDecode($res->getBody()->getContents());
-    }
-
-    private function query($endpoint)
-    {
-        return $this->request('GET', $this->endpoint . '/' . $endpoint);
-    }
-
-    private function sendTransaction($method, $endpoint, $params, $gasInfo)
-    {
-        $url = $this->endpoint . '/' . $endpoint;
-
-        $txn = $this->request($method, $url, [ 
-            'json' => $params
-        ])['value'];
-
-        $txn = $this->updateGasAndFee($txn, $gasInfo);
-
-        $txn['memo'] = Utils::makeRandomString();
-
-        Utils::ksortRecursive($txn);
-
-        $txn['signatures'] = [[
-            'account_number' => (string) $this->accountInfo['account_number'],
-            'pub_key' => [
-                'type' => 'tendermint/PubKeySecp256k1',
-                'value' => Utils::base64Encode(Utils::decodeHex($this->getPublicKey()))
-            ],
-            'sequence' => (string) $this->accountInfo['sequence'],
-            'signature' => $this->signTransaction($txn)
-        ]];
-
-        return $this->broadcastTransaction($method, 'txs', $txn);
-    }
-
-    private function broadcastTransaction($method,  $endpoint, $txn)
-    {
-        $url = $this->endpoint . '/' . $endpoint;
-
-        $res = $this->request($method, $url, [
-            'json' => [
-                'mode' => 'block',
-                'tx' => $txn
-            ]
-        ]);
-
-        if (isset($res['code'])) {
-            throw new \Exception($res['raw_log']);
-        } else {
-            return Utils::jsonDecode(Utils::decodeHex($res['data']));
-        }
-    }
-
-    private function signTransaction($txn)
-    {
-        $data = [
-            'account_number' => (string) $this->accountInfo['account_number'],
-            'chain_id' => $this->chainId,
-            'fee' => $txn['fee'],
-            'memo' => $txn['memo'],
-            'msgs' => $txn['msg'],
-            'sequence' => (string) $this->accountInfo['sequence']
-        ];
-
-        return Utils::base64Encode($this->signData(Utils::jsonEncode($data)));
-    }
-
-    private function signData($data)
-    {
-        $ec = new EC('secp256k1');
-
-        $key = $ec->keyFromPrivate($this->getPrivateKey());
-        
-        $hash = Utils::sha256Digest($data);
-        
-        $sig = $key->sign($hash, 'hex', [ 'canonical' => true ]);
-
-        $arr = array_merge($sig->r->toArray('be', 32), $sig->s->toArray('be', 32));
-
-        return \join(\array_map('chr', $arr));
-    }
-
-    private function getPrivateKey()
-    {
-        return $this->key->getPrivateKey()->getHex();
-    }
-
-    private function getPublicKey()
-    {
-        return $this->key->getPublicKey()->getHex();
-    }
-
-    private function updateGasAndFee($txn, $gasInfo)
-    {
-
-        $gas = (int) $txn['fee']['gas'];
-        $maxGasParam = null;
-        $maxPriceParam = null;
-
-        if (isset($gasInfo['max_fee'])) {
-            $maxGasParam = (int) $gasInfo['max_fee'];
-        }
-
-        if (isset($gasInfo['gas_price'])) {
-            $maxPriceParam = (int) $gasInfo['gas_price'];
-        }
-
-        
-        if ($gas > $maxGasParam) {
-            $txn['fee']['gas'] = $maxGasParam;
-        }
-
-        if ($maxGasParam) {
-            $txn['fee']['amount'] = [[
-                'denom' => 'ubnt',
-                'amount' => (string) $maxGasParam 
-            ]];
-        } else if ($maxPriceParam) {
-            $txn['fee']['amount'] = [[
-                'denom' => 'ubnt',
-                'amount' => (string) ($gas * $maxPriceParam)
-            ]];
-        }
-
-        return $txn;
-    }
 
     private function buildParams($params)
     {
---
+++ src/Cosmos.php
@@ -0,0 +1,189 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bluzelle\Bluzelle;
+
+use \GuzzleHttp\Client as HttpClient;
+use \Elliptic\EC;
+
+class Cosmos
+{
+    private $endpoint;
+    private $chainId;
+    private $httpClient;
+    private $accountInfo;
+    private $key;
+
+    public function __construct($address, $mnemoic, $endpoint, $chainId)
+    {
+        $this->address = $address;
+        $this->endpoint = $endpoint;
+        $this->chainId = $chainId;
+        $this->accountInfo = [];
+        $this->httpClient = new HttpClient();
+        $this->key = Utils::getECKey($mnemoic);
+
+        $this->validateAddress();
+        $this->setAccountDetails();
+    }
+
+    public function query($endpoint)
+    {
+        return $this->request('GET', $this->endpoint . '/' . $endpoint);
+    }
+
+    public function sendTransaction($method, $endpoint, $params, $gasInfo)
+    {
+        $url = $this->endpoint . '/' . $endpoint;
+
+        $txn = $this->request($method, $url, [ 
+            'json' => $params
+        ])['value'];
+
+        $txn = $this->updateGas($txn, $gasInfo);
+
+        $txn['memo'] = Utils::makeRandomString();
+
+        Utils::ksortRecursive($txn);
+
+        $txn['signatures'] = [[
+            'account_number' => (string) $this->accountInfo['account_number'],
+            'pub_key' => [
+                'type' => 'tendermint/PubKeySecp256k1',
+                'value' => Utils::base64Encode(Utils::decodeHex($this->getPublicKey()))
+            ],
+            'sequence' => (string) $this->accountInfo['sequence'],
+            'signature' => $this->signTransaction($txn)
+        ]];
+
+        return $this->broadcastTrasnaction($method, 'txs', $txn);
+    }
+
+    public function setHttpClient($httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    private function broadcastTrasnaction($method, $endpoint, $txn)
+    {
+        $url = $this->endpoint . '/' .$endpoint;
+
+        $res = $this->request($method, $url, [
+            'json' => [
+                'mode' => 'block',
+                'tx' => $txn
+            ]
+        ]);
+
+        if (isset($res['code'])) {
+            throw new \Exception($res['raw_log']);
+        } else {
+            return Utils::jsonDecode(Utils::decodeHex($res['data']));
+        }
+    }
+
+    private function request($method, $url, $data = []): array
+    {
+        $res = $this->httpClient->request($method, $url, $data);
+        return Utils::jsonDecode($res->getBody()->getContents());
+    }
+
+    private function validateAddress()
+    {
+        $retrievedAddress = Utils::getAddressFromPublicKey($this->getPublicKey());
+
+        if ($this->address !== $retrievedAddress)
+            throw new Exception\AddressValidationFailedException();
+    }
+
+    private function signTransaction($txn)
+    {
+        $data = [
+            'account_number' => (string) $this->accountInfo['account_number'],
+            'chain_id' => $this->chainId,
+            'fee' => $txn['fee'],
+            'memo' => $txn['memo'],
+            'msgs' => $txn['msg'],
+            'sequence' => (string) $this->accountInfo['sequence']
+        ];
+
+        return Utils::base64Encode(
+            $this->generateSignature(Utils::jsonEncode($data))
+        );
+    }
+
+    private function generateSignature(string $data): string
+    {
+        $ec = new EC('secp256k1');
+        $key = $ec->keyFromPrivate($this->getPrivateKey());
+
+        $hash = Utils::sha256Digest($data);
+        $sig = $key->sign($hash, 'hex', [ 'canonical' => true ]);
+
+        $bArr = array_merge(
+            $sig->r->toArray('be', 32),
+            $sig->s->toArray('be', 32)
+        );
+
+        return join(array_map('chr', $bArr));
+    }
+
+    private function setAccountDetails()
+    {
+        $data = $this->account();
+
+        $this->accountInfo['account_number'] = $data['account_number'];
+        $this->accountInfo['sequence'] = $data['sequence'];
+    }
+
+    private function account()
+    {
+        $url = 'auth/accounts/' . $this->address;
+        return $this->query($url)['result']['value'];
+    }
+
+    private function getPrivateKey(): string
+    {
+        return $this->key->getPrivateKey()->getHex();
+    }
+
+    private function getPublickey(): string
+    {
+        return $this->key->getPublickey()->getHex();
+    }
+
+    private function updateGas(array $txn, array $gasInfo): array
+    {
+        $gas = (int) $txn['fee']['gas'];
+        $maxGasParam = null;
+        $maxPriceParam = null;
+
+        if (isset($gasInfo['max_fee'])) {
+            $maxGasParam = (int) $gasInfo['max_fee'];
+        }
+
+        if (isset($gasInfo['gas_price'])) {
+            $maxPriceParam = (int) $gasInfo['gas_price'];
+        }
+
+        
+        if ($gas > $maxGasParam) {
+            $txn['fee']['gas'] = $maxGasParam;
+        }
+
+        if ($maxGasParam) {
+            $txn['fee']['amount'] = [[
+                'denom' => 'ubnt',
+                'amount' => (string) $maxGasParam 
+            ]];
+        } else if ($maxPriceParam) {
+            $txn['fee']['amount'] = [[
+                'denom' => 'ubnt',
+                'amount' => (string) ($gas * $maxPriceParam)
+            ]];
+        }
+
+        return $txn;
+    }
+}
